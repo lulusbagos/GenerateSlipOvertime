e628e134830848926ee4a73a47f23a1c361ac0cc
Wahyu Susanti <p61181444@pamapersada.net>
2022-12-22 14:40:32 +0700

    First Commit

---
+++ CConection.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.Common
+Imports System.Configuration
+
+Public Class CConnection
+    Private pv_i_conn As SqlConnection
+    Private pv_i_cmd As SqlCommand
+
+    Public Sub New()
+        Me.pv_cust_setConnection()
+    End Sub
+
+    Private Sub pv_cust_setConnection()
+        Dim connectionstring As String = ConfigurationSettings.AppSettings("PERSISConstring").ToString
+        pv_i_conn = New SqlConnection(connectionstring)
+    End Sub
+
+    Public Function pb_cust_getConnection() As SqlConnection
+        Return pv_i_conn
+    End Function
+
+    Public Sub pb_cust_closeConnection()
+        Me.pv_i_conn.Close()
+    End Sub
+
+    Public Function pb_cust_executeNonQuery(ByVal s_str_query As String, ByRef s_str_remark As String) As Boolean
+        Dim i_bol_return As Boolean = False
+        Try
+            pv_i_conn.Open()
+            pv_i_cmd = New SqlCommand(s_str_query, pv_i_conn)
+            pv_i_cmd.ExecuteNonQuery()
+            pv_i_conn.Close()
+            pv_i_cmd.Dispose()
+            i_bol_return = True
+            s_str_remark = "Query Executed"
+        Catch ex As Exception
+            i_bol_return = False
+            s_str_remark = ex.Message.ToString
+        End Try
+        Return True
+    End Function
+
+    Public Function pb_cust_executeQuery(ByVal s_str_query As String) As DataTable
+        Dim i_dtab_return As New DataTable
+        Dim i_tdap As New SqlDataAdapter
+
+        pv_i_conn.Open()
+        pv_i_cmd = New SqlCommand(s_str_query, pv_i_conn)
+        i_tdap.SelectCommand = pv_i_cmd
+        i_tdap.Fill(i_dtab_return)
+
+        pv_i_conn.Close()
+        pv_i_cmd.Dispose()
+
+        Return i_dtab_return
+    End Function
+
+    Public Function pb_cust_executeScalar(ByVal s_str_query As String) As String
+        Dim i_str_return As String
+
+        pv_i_conn.Open()
+        pv_i_cmd = New SqlCommand(s_str_query, pv_i_conn)
+
+        If IsDBNull(pv_i_cmd.ExecuteScalar()) Then
+            i_str_return = "0"
+        Else
+            i_str_return = pv_i_cmd.ExecuteScalar()
+        End If
+
+        pv_i_conn.Close()
+        pv_i_cmd.Dispose()
+
+        Return i_str_return
+    End Function
+End Class
---
+++ LotusNotes.vb
@@ -0,0 +1,46 @@
+﻿Imports System.IO
+Imports System.Net
+Imports iTextSharp
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.Configuration
+Module LotusNotes
+
+
+    Dim docobj As Object
+
+
+    Sub Main()
+        Dim cls As New CConnection
+        Dim qry As String
+        Dim remark As String = ""
+        Dim dt As New DataTable
+        Dim periode As String
+
+
+
+        qry = ConfigurationSettings.AppSettings("query").ToString
+
+        dt = cls.pb_cust_executeQuery(qry)
+        For i As Integer = 0 To dt.Rows.Count - 1
+            pv_cust_download(dt.Rows(i).Item("file_name").ToString.ToString, dt.Rows(i).Item("nrp").ToString.ToString, dt.Rows(i).Item("periode").ToString.ToString)
+        Next
+        ' pv_cust_getList()
+
+    End Sub
+
+    Private Sub pv_cust_download(file_name As String, nrp As String, periode As String)
+        Try
+            Dim patch_download As String = ConfigurationSettings.AppSettings("patch_download").ToString
+            'patch_download = patch_download & "\" & periode
+
+            Dim report As String = "http://10.2.183.120/ReportServer/Pages/ReportViewer.aspx?%2fIPO%2fRpt_Insentif_Produksi_FPTNew&INSENTIF_MONTHLY_PID=" & periode & "&rs:Command=Render&rs%3AFormat=PDF"
+            Dim client As New WebClient()
+            client.UseDefaultCredentials = True
+            client.DownloadFile(New Uri(report.ToString), patch_download & "\" & file_name & ".pdf")
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+        End Try
+    End Sub
+
+End Module
